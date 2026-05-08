13e9580c1bd5909ff79826997e50423381bb1b1e
SanjayGrover <sanjaygrover2005@gmail.com>
2026-05-08 16:52:27 +0530

    Prepare front-end for deployment

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ const itemRoutes = require('./routes/items');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/lists', listRoutes);
