3563e654916a6f060c2b303657ce943bab797ca4
SanjayGrover <sanjaygrover2005@gmail.com>
2026-05-08 13:58:48 +0530

    Client: Adding the drag and drop for Items

---
+++ client/src/components/SortableItem.jsx
@@ -0,0 +1,27 @@
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+
+const SortableItem = ({ id, children }) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    opacity: isDragging ? 0.4 : 1,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style}>
+      {children({ dragHandleProps: { ...attributes, ...listeners } })}
+    </div>
+  );
+};
+
+export default SortableItem;
