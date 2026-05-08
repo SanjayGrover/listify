219a03438f9121f9f4473c555e8e70d23218e52d
SanjayGrover <sanjaygrover2005@gmail.com>
2026-05-08 19:27:34 +0530

    Server: setup environment variables and server side reset password setup

---
+++ server/utils/sendEmail.js
@@ -0,0 +1,20 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async ({ to, subject, html }) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_APP_PASSWORD,
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"Listify" <${process.env.GMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
+};
+
+module.exports = sendEmail;
